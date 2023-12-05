407c630b401594856595153c47ec98f564928c51
bayupriyambada17 <bayupm.dev@gmail.com>
2023-12-05 09:30:45 +0700

    add

--- nuxt.config.js
+++ nuxt.config.js
@@ -159,7 +159,8 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'http://localhost:8000',
+    // baseURL: 'http://localhost:8000',
+    baseURL: 'https://api.ppdb.smktibazma.sch.id',
     credentials: true,
 
   },
